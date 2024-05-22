5aab43b7dc4c2808591727da92c7f587a769f892
Huy Pham <pbhuy0909@gmail.com>
2024-05-22 17:43:45 +0700

    [fix] test failed

--- express_service/services/job/utils/server-test.js
+++ express_service/services/job/utils/server-test.js
@@ -1,11 +1,11 @@
 // express-app.js
 const express = require('express');
 const bodyParser = require('body-parser');
-const routers = require('./routers');
-const StartSubscriber = require('./utils/pubsub_rabbitmq/jobs/sub');
-const grpc = require('@grpc/grpc-js');
-const grpcJobServer = require('./grpc-server');
-const { GRPC_JOB_SERVER } = require('./configs');
+const routers = require('../routers');
+// const StartSubscriber = require('./utils/pubsub_rabbitmq/jobs/sub');
+// const grpc = require('@grpc/grpc-js');
+// const grpcJobServer = require('./grpc-server');
+// const { GRPC_JOB_SERVER } = require('./configs');
 
 const createServer = async () => {
   const app = express();
@@ -16,15 +16,15 @@ const createServer = async () => {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(express.static(__dirname + '/public'));
 
-  StartSubscriber();
+  // StartSubscriber();
 
-  grpcJobServer.bindAsync(GRPC_JOB_SERVER, grpc.ServerCredentials.createInsecure(), (err, port) => {
-    if (err) {
-      console.error('Server bind failed:', err);
-    } else {
-      console.log('gRPC server for Service Transaction running on port', port);
-    }
-  });
+  // grpcJobServer.bindAsync(GRPC_JOB_SERVER, grpc.ServerCredentials.createInsecure(), (err, port) => {
+  //   if (err) {
+  //     console.error('Server bind failed:', err);
+  //   } else {
+  //     console.log('gRPC server for Service Transaction running on port', port);
+  //   }
+  // });
 
   routers(app);
 
